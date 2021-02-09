7b022d018cd9c85984b53ce41719f675e88fdcc9
AyumuNekozuki <ayumu@nekozuki.me>
2021-02-09 15:36:20 +0900

    リメイクのために整理

--- firebase_js/firebase_init.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-  apiKey: 'AIzaSyC0IHYaMM7BAlNt6aYrW7URnGWrfs5AlzQ',
-  projectId: 'virtualcast-newspaper',
-  storageBucket: 'virtualcast-newspaper.appspot.com'
-});
-
-const db = firebase.firestore();
-const storage = firebase.storage();
-const storageRef = storage.ref();
--- firebase_js/new_read.js
+++
@@ -1,8 +0,0 @@
-const archives_db = db.collection("archives");
-archives_db.orderBy("date", "desc").limit(1).get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    var id = doc.id;
-    var url = 'https://vcnp.nekozuki.me/archives/'+ id + '/';
-    location.href = url;
-  });
-});
