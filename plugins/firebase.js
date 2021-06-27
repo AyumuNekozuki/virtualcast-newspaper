46a559eb525963236cc9462a87a9bbd1e3245fd5
AyumuNekozuki <ayumu@nekozuki.me>
2021-06-27 19:00:59 +0900

    fix

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,13 +5,13 @@ import storage from 'firebase/storage'
 if (!firebase.apps.length) {
   firebase.initializeApp(
     {
-      apiKey: "AIzaSyC0IHYaMM7BAlNt6aYrW7URnGWrfs5AlzQ",
+      apiKey: "***",
       authDomain: "virtualcast-newspaper.firebaseapp.com",
       projectId: "virtualcast-newspaper",
       storageBucket: "virtualcast-newspaper.appspot.com",
-      messagingSenderId: "807682455880",
-      appId: "1:807682455880:web:e4e8a4b50bf94ba3475b7d",
-      measurementId: "G-T446LZ02QS"
+      messagingSenderId: "***",
+      appId: "***",
+      measurementId: "G-***"
     }
   )
 }
